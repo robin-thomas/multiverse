0481f8b49932dd54e13dac6f61bea5137352e927
Robin Thomas <robinthomas2591@gmail.com>
2020-07-08 22:14:39 +0800

    improve profile page

--- src/components/pages/profile/About.js
+++ src/components/pages/profile/About.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { MDBBtn, MDBIcon } from 'mdbreact';
 import { Card, Button } from 'react-bootstrap';
 
 import styles from './About.module.css';
 
 const About = () => {
+  const [overlay, setOverlay] = useState(false);
+
   return (
     <Card className={styles['about-card']}>
       <Card.Img
         variant="top"
-        className={styles['about-pic']}
+        className={styles[`about-pic${overlay ? '-overlay-trigger' : ''}`]}
         src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+        onMouseEnter={() => setOverlay(true)}
       />
+      {overlay ? (
+        <Card.ImgOverlay onMouseLeave={() => setOverlay(false)}>
+          <MDBBtn
+            outline
+            color="primary"
+            title="Edit your picture"
+            className={styles['about-edit-pic-icon']}
+          >
+            <MDBIcon icon="edit" />
+          </MDBBtn>
+        </Card.ImgOverlay>
+      ) : null}
       <Card.Body>
         <Card.Title>ABOUT</Card.Title>
         <Card.Text>
